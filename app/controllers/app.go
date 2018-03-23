ff80650ef65005659c079e67da5638583ac18d3e
lichunyan <vamlichunyan@foxmail.com>
2018-03-23 17:19:51 +0800

    new a project

---
+++ app/controllers/app.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"github.com/revel/revel"
+)
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+
+func (c App) Healthcheck() revel.Result{
+	return c.Render()
+}
